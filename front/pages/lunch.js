5069a813763462634c72fa01639b9fcf6ece8197
briiiqtt <briiiqtt@gmail.com>
2022-10-24 10:55:39 +0900

    이게아닌디

--- front/pages/lunch.js
+++ front/pages/lunch.js
@@ -9,7 +9,11 @@ const Lunch = () => {
     fetch(`http://10.0.0.15:59999/lunch-fetch`)
       .then((r) => r.json())
       .then((r) => {
-        for (let a of r.result_data?.items) {
+        if (!r.result_data.items) {
+          alert('요청 실패');
+          return;
+        }
+        for (let a of r.result_data.items) {
           if (a.author?.name === '두레비앙-정소예') {
             tempArr.push(<img src={a.photos[0].url}></img>);
           }
@@ -24,7 +28,9 @@ const Lunch = () => {
 
   return (
     <>
-      <div ref={lunchDiv} style={{ textAlign: 'center' }}>{imgArr}</div>
+      <div ref={lunchDiv} style={{ textAlign: 'center' }}>
+        {imgArr}
+      </div>
     </>
   );
 };
